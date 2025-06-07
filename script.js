00b825a9df5462061e933b680861e48c808e00ef
Ajayi Ayanfe <ajayiayanfe9@gmail.com>
2025-06-07 18:51:35 +0100

    removed codes used for testing

--- script.js
+++ script.js
@@ -2,14 +2,8 @@ const userInput = document.getElementById("user-input");
 const checkBtn = document.getElementById("check-btn");
 const clearBtn = document.getElementById("clear-btn");
 const resultsDiv = document.getElementById("results-div");
-const test = ["1 555)555-5555", "555)-555-5555", "(555-555-5555", "1 555-555-5555", "1 (555) 555-5555", "1(555)555-5555", "1 555 555 5555", "5555555555", "555-555-5555", "(555)555-5555"];
 
 const validNumberRegex = /^1?\s?((\([0-9][0-9][0-9]\))|([0-9][0-9][0-9]))([\s\-])?([0-9][0-9][0-9])([\s\-])?([0-9][0-9][0-9][0-9])$/
-
-test.forEach((number) => {
-    const match = validNumberRegex.test(number);
-    console.log(number, "matched?", match);
-});
 
 const checkUserInput = (str) => {
     if (str === "") {
